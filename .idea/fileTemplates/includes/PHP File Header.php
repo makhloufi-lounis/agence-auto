87769dad4024e72c0c775895b802e0356bdaa9bb
Lounis Makhloufi <makhloufi.lounis@gmail.com>
2020-09-02 23:51:15 +0200

    init : project installation and configuration

---
+++ .idea/fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,9 @@
+/**
+ * API Shop
+ *
+ * @version   1.0
+ * @author    Lounis Makhloufi <makhloufi.lounis@gmail.com>
+ * @see       https://github.com/makhloufi-lounis/agence-auto for the canonical source repository
+ * @copyright Copyright (c) 2020. 
+ */
+ 
